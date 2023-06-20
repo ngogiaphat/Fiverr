2d8df90c7627973e0ca86194dfec976815de59a3
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-20 20:47:47 +0700

    order older

---
+++ client/src/pages/seller/orders/message/[orderld].jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MessageContainer from "../../../../components/Messages/MessageContainer";
+function Message(){
+  return(
+    <div>
+      <MessageContainer/>
+    </div>
+  );
+};
+export default Message;
