534694867b447195f7b83bcdd2376cefa0352e57
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-21 14:27:45 +0700

    index Home Page

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AuthWrapper from "../components/AuthWrapper";
+import Services from "../components/Landing/Services";
+import Companies from "../components/Landing/Companies";
+import {useStateProvider} from "../context/StateContext";
+import Everything from "../components/Landing/Everything";
+import HeroBanner from "../components/Landing/HeroBanner";
+import JoinFiverr from "../components/Landing/JoinFiverr";
+import FiverrBusiness from "../components/Landing/FiverrBusiness";
+import PopularServices from "../components/Landing/PopularServices";
+function Index(){
+	const [{showLoginModal, showSignupModal}] = useStateProvider();
+	return(
+		<div>
+			<Services/>
+			<Companies/>
+			<HeroBanner/>
+			<Everything/>
+			<JoinFiverr/>
+			<FiverrBusiness/>
+			<PopularServices/>
+			{(showLoginModal || showSignupModal) && (
+				<AuthWrapper type = {showLoginModal ? "login" : "signup"}/>
+			)}
+		</div>
+	);
+};
+export default Index;
